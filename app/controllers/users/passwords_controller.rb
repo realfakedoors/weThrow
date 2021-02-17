2b61400acd0f3c2dea7bfa9f15ec654df42e3c88
Andy Holt <andyholt85@gmail.com>
2021-02-17 01:58:04 -0700

    sanitize auth controller params

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,2 +1,14 @@
 class Users::PasswordsController < Devise::PasswordsController
+  before_action :configure_send_password_reset_params, only: [:create]
+  before_action :configure_update_passwords_params, only: [:update]
+
+  protected
+
+  def configure_send_password_reset_params
+    devise_parameter_sanitizer.permit(:password, keys: [:email])
+  end
+  
+  def configure_update_passwords_params
+    devise_parameter_sanitizer.permit(:password, keys: [:password, :password_confirmation, :reset_password_token])
+  end
 end
