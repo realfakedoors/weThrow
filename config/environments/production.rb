e389f83fe5db9cfc05c30866880a52d6d3e7e729
Andy Holt <andyholt85@gmail.com>
2021-04-25 17:55:58 -0600

    update production configuration for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   
   # Define a default mailer host.
-  config.action_mailer.default_options = { from: 'weThrow' }
+  config.action_mailer.default_url_options = { host: 'https://wethrow.herokuapp.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
